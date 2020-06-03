bc95c5a69e89695f5c36ef80b69caea81bfd9680
Jesus <jes.librado@gmail.com>
2020-06-03 11:22:04 -0500

    Tried adding async request in storybook

--- student/src/stories/Map.stories.js
+++ student/src/stories/Map.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Map from '../components/Map';
+import axios from 'axios';
 
 export default {
     title: 'Map',
@@ -7,6 +8,7 @@ export default {
 };
 
 const params = {
+    markers: [],
     viewport: {
         latitude: 19.023840,
         longitude: -98.243571,
@@ -19,7 +21,15 @@ const params = {
         longitude: -98.243571
     }
 };
+
+const stops = {
+    markers: []
+}
+
+axios.get(`http://${process.env.REACT_APP_API_URL}/locations/near?longitude=-98.243571&latitude=19.023840`)
+.then(res=>res.data)
+.then(array=>stops.markers = array);
   
 export const Default = () => <Map {...params} />
 
-export const Stops = () => <Map {...params} />
+export const Stops = () => <Map {...params} {...stops}/>
