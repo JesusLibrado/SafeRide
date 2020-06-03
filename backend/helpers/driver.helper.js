e679631ae8f562f3f4d5d9eb47bbc2cc88a24887
A01172971 <A01172971@itesm.mx>
2020-06-03 01:04:58 -0500

    trips son creados

--- backend/helpers/driver.helper.js
+++ backend/helpers/driver.helper.js
@@ -1,20 +1,19 @@
 const Driver = require('../models/Driver');
+const University = require('../models/University');
 
 exports.getDriverById = async (id, fields='') =>{
     return await Driver.findById(id, fields);
 }
 
 exports.getDriverUniversity = async (id, uni_fields='') =>{
-    let driver = await Driver.findById(id)
-    .populate({
-        path: 'student', 
-        populate: {
-            path: 'university', 
-            select: uni_fields
-        }
-    });
+    let driver = await Driver.findOne({"student": id})
+    .populate('student')
+    .populate('university');
     if(driver) {
-        return driver.student.university;
+        let university =  await University.findById( driver.student.university);
+        console.log(university);
+
+        return university;
     }
     else {
         return null;
